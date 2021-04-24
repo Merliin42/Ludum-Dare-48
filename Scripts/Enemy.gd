7760773cc09cfb63094c52f6d8026cbabc1a11b9
Lucas Dupuis <32811851+Merliin42@users.noreply.github.com>
2021-04-24 13:56:58 +0200

    Modif scenes

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
-
+export var time : int = 1
 export var direction : Vector2 = Vector2(0, 0)
 export var SPEED : int = 200
+
+func _ready():
+	$Timer.wait_time = time
+	$Timer.start()
 
 func _process(delta):
 	position += direction.normalized()*SPEED*delta
---
+++ Scripts/Main.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+func _process(delta):
+#	print($Level2.visible)
+	pass
