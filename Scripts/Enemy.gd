88e5d6b1e9298349ddc67fb7f414200245e49666
Shae1507 <37331677+Shae1507@users.noreply.github.com>
2021-04-25 15:02:33 +0200

    ajout poissons

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -3,6 +3,7 @@ extends Area2D
 export var time : int = 1
 export var direction : Vector2 = Vector2(0, 0)
 export var SPEED : int = 200
+onready var _animated_sprite = $AnimatedSprite
 
 func _ready():
 	$Timer.wait_time = time
@@ -10,7 +11,12 @@ func _ready():
 
 func _process(delta):
 	position += direction.normalized()*SPEED*delta
+	if direction.x <= 0 :
+		_animated_sprite.play("moveLeft")
+	else:
+		_animated_sprite.play("moveRight")
 
 
 func _on_Timer_timeout():
 	direction = -direction
+
